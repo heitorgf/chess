3b9f6290d2a6b38261121da348756896bc953647
heitorgf <heitor1028@hotmail.com>
2021-11-06 20:01:01 -0300

    Aula 08: Movimentos e funções

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,25 @@
 package application;
 
 import rulechess.ChessMatch;
-
+import rulechess.ChessPiece;
+import rulechess.Chessposition;
+import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 	ChessMatch chessMatch = new ChessMatch();
+	
+	while(true) {
 	UI.printBoard(chessMatch.getPieces());
+	System.out.println();
+	System.out.print("Source: ");
+	Chessposition source = UI.readChessPosition(sc);
+	System.out.println();
+	System.out.print("Target: ");
+	Chessposition target= UI.readChessPosition(sc);
+	
+	ChessPiece capturedPiece= chessMatch.performChessMove(source, target);
+}
 }
 }
--- src/rulechess/ChessMatch.java
+++ src/rulechess/ChessMatch.java
@@ -1,6 +1,8 @@
 package rulechess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import rulechess.pieces.King;
 import rulechess.pieces.Rook;
 
@@ -20,6 +22,26 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	public ChessPiece performChessMove(Chessposition sourcePosition, Chessposition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		validateSourcePosition(source);
+		Piece capturedPiece= makeMove(source, target);
+		return (ChessPiece)capturedPiece;
+	}
+	private Piece makeMove(Position source, Position target) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return (ChessPiece)capturedPiece;
+	}
+	
+	private void validateSourcePosition(Position position) {
+		if (!board.thereIsAPiece(position)) {
+			throw new ChessException("Threre is no piece on source position");
+		}
 	}
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 			board.placePiece(piece, new Chessposition(column, row).toPosition());
